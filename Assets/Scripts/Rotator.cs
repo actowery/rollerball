40b60c810fd9b77396b06ef2b4b80617fa864fe8
Cihan Adrian Towery <adrian.towery@gmail.com>
2017-12-28 16:24:08 -0500

    load to github

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// ...also rotate around the World's Y axis
+		transform.Rotate(new Vector3(15,30,15)*Time.deltaTime);
+	}
+}
